b31c35a127209f8c5c8cda0d4931d1218c344bff
PaoloLuciano <paoluciano@prodigy.net.mx>
2018-01-16 14:54:48 -0600

    Modelo todavia en pruebas pero practicamente funcional

--- Modelo/ModeloMNP_CoahuilaTest.R
+++ Modelo/ModeloMNP_CoahuilaTest.R
@@ -4,6 +4,9 @@
 library(dplyr)
 library(nnet)
 library(MNP)
+library(coda)
+library(bayesm)
+library(mlogit)
 
 inegi <- read.csv("../inegi/out/inegi_summary.csv", header = TRUE)
 estados <- read.csv("key_estados.csv")
@@ -66,12 +69,63 @@ datos_mod <- datos_mod[complete.cases(datos_mod), ]
 
 write.csv(datos_mod, "datos_modelo_coahuila.csv", row.names = FALSE)
 
+# Estandarizo los datos para evitar problemas numéricos (creo que no se necesita tbh)
+cols_indices <- 1:5
+datos_mod_esc <- datos_mod
+datos_mod_esc[, -cols_indices] <- scale(datos_mod_esc[, -cols_indices])
+
+# Tamaño de la muestra para usar para entrenar
+p <- .9
+index <- sample(1:dim(datos_mod_esc)[1], size = dim(datos_mod_esc)[1]*p)
+train <- datos_mod_esc[index, ]
+test <- datos_mod_esc[-index, ]
+
 # SETUP DEL MODELO
-# example(mnp)
+modelo_coahuila <- mnp(GANADOR ~ HIJOS + LIMITACION + ANALFABETISMO + 
+                    EDUCACION_AV + NO_SERV_SALUD + AUTO, 
+                    data = datos_mod_esc, 
+                    base = "CPRI",
+                    n.draws = 50000,
+                    burnin = 10000,
+                    thin = 4,
+                    verbose = TRUE,
+                    latent = TRUE)
+summary(modelo_coahuila)
 
-# Hay que ver todo lo de las priors (encuestas)
-modelo_coahuila <- mnp(data = ) 
+# Análisis de convergencia
+run1 <- mnp(GANADOR ~ HIJOS + LIMITACION + ANALFABETISMO + 
+                EDUCACION_AV + NO_SERV_SALUD + AUTO, 
+            data = datos_mod_esc, 
+            base = "CPRI",
+            n.draws = 10000,
+            verbose = TRUE)
+
+run2 <- mnp(GANADOR ~ HIJOS + LIMITACION + ANALFABETISMO + 
+                EDUCACION_AV + NO_SERV_SALUD + AUTO, 
+            data = datos_mod_esc, 
+            base = "CPRI",
+            n.draws = 10000,
+            verbose = TRUE,
+            coef.start = 10*c(1,-1)*seq(1, times =21),
+            cov.start = matrix(0.5, ncol = 3, nrow = 3) + diag(0.5, 3))
+
+run3 <- mnp(GANADOR ~ HIJOS + LIMITACION + ANALFABETISMO + 
+                EDUCACION_AV + NO_SERV_SALUD + AUTO, 
+            data = datos_mod_esc, 
+            base = "CPRI",
+            n.draws = 10000,
+            verbose = TRUE,
+            coef.start = 10*c(-1,1)*seq(1, times =21),
+            cov.start = matrix(0.9, ncol = 3, nrow = 3) + diag(0.9, 3))
+
+res.coda <- mcmc.list(cadena1 = mcmc(run1$param),
+                      cadena2 = mcmc(run2$param),
+                      cadena2 = mcmc(run3$param))
+
+gelman.diag(res.coda, transform = TRUE)
+gelman.plot(res.coda, transform = TRUE, ylim = c(1,1.2))
 
 
-
-
+# Algúnas gráficas
+hist(modelo_coahuila$param[,1])
+plot(modelo_coahuila$param[,3], type = 'l')
