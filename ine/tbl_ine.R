b3a5951c05ca00453827b6447d97989730cbbe92
Mariana Franklin <mariana.r.franklin@gmail.com>
2018-01-30 19:42:04 -0600

    Check output folder

--- ine/tbl_ine.R
+++ ine/tbl_ine.R
@@ -10,27 +10,7 @@ require(jsonlite)
 require(stringr)
 
 # FUN
-cleanText <- function(text) {
-  text <- str_replace_all(text, 'á', 'a')
-  text <- str_replace_all(text, 'é', 'e')
-  text <- str_replace_all(text, 'í', 'i')
-  text <- str_replace_all(text, 'ó', 'o')
-  text <- str_replace_all(text, 'ú', 'u')
-  text <- str_replace_all(text, 'ü', 'u')
-  text <- str_replace_all(text, 'ñ', 'n')
-  text <- str_replace_all(text, '\\.', '')
-  text <- gsub('(?<=[\\s])\\s*|^\\s+|\\s+$', '', text, perl = TRUE)
-    # checks for whitespace - deserves its own explanation:
-    # (?<=    look behind to see if there is
-    # [\s]    any character of: whitespace (\n, \r, \t, \f, and ' ')
-    # )       end of look behind
-    # \s*     whitespace (\n, \r, \t, \f, and ' ') (0 or more times (matching the most amount possible))
-    # |       or
-    # ^       the beginning of the string
-    # \s+     whitespace (\n, \r, \t, \f, and ' ') (1 or more times (matching the most amount possible))
-    # $       before an optional \n, and the end of the string
-  return(text)
-}
+
 
 # DATA
 
@@ -159,4 +139,4 @@ df <- tbl %>%
     everything()) %>%
   arrange(ANO, ELECCION, CODIGO_ESTADO, SECCION)
 
-write.csv(df, 'dat/out_ine.csv', row.names = F)
+write.csv(df, 'out/tbl_ine.csv', row.names = F)
