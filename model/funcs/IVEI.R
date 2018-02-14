8d5ad2e71c7eededb613e838250d50fbc75cb90b
PaoloLuciano <paoluciano@prodigy.net.mx>
2018-02-14 14:05:21 -0600

    Pruebas IVEI

--- model/funcs/IVEI.R
+++ model/funcs/IVEI.R
@@ -1,8 +1,8 @@
 # Función que calcula el ÍNDICE DE VOLATILIDAD ELECTORAL IRRATIONAL® para una matriz de AFINIDAD POLITICA®
 
 IVEI <- function(X, p = 2){
-    # X matriz de AFINIDAD POLíTICA de n x m donde
-    # n = número de secciones
+    # X data frame de AFINIDAD POLíTICA de n x m donde
+    # n = número de secciones/estados
     # m = número de partidos
     # p = tipo de norma a usar
     
@@ -10,13 +10,13 @@ IVEI <- function(X, p = 2){
         m <-  dim(X)[2]
         
         alpha <- m^((1-p)/p)
-        print(alpha)
+        # print(alpha)
         Y <- apply(X, 1, function(x) sum(abs(x)^p)^(1/p))
-        print(Y)
-        return(Y/(alpha - 1) - 1/(alpha - 1))    
+        # print(Y)
+        return(as.matrix(Y/(alpha - 1) - 1/(alpha - 1)))    
     } 
     else{
-        stop()
+        stop("p debe ser mayor a 1")
     }
 }
 
